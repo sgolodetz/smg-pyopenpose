ec688c1aa9292301d83844cb8e42aa360827b097
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-03 19:53:50 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="smg-pyopenpose",
+    version="0.0.1",
+    author="Stuart Golodetz",
+    author_email="stuart.golodetz@cs.ox.ac.uk",
+    description="Interface to OpenPose",
+    long_description="",  #long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgolodetz/smg-pyopenpose",
+    packages=["smg.pyopenpose"],
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "opencv-contrib-python==3.4.2.16"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='==3.7.*',
+)
