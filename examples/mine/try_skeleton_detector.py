5270934c7730ba05c1a6607d3160e134a36243ca
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-05 15:44:16 +0000

    Continue commenting and tidying.

--- examples/mine/try_skeleton_detector.py
+++ examples/mine/try_skeleton_detector.py
@@ -54,59 +54,79 @@ def main() -> None:
         )
 
     try:
+        # Construct the skeleton detector.
         params: Dict[str, Any] = {"model_folder": "D:/openpose-1.6.0/models/"}
         with SkeletonDetector(params) as skeleton_detector:
+            # Construct the camera.
             with OpenNICamera(mirror_images=True) as camera:
+                # Construct the bone length estimator.
                 bone_length_estimator: BoneLengthEstimator = BoneLengthEstimator()
 
+                # Repeatedly:
                 while True:
                     # Process any PyGame events.
                     for event in pygame.event.get():
+                        # If the user wants us to quit:
                         if event.type == pygame.QUIT:
+                            # Shut down pygame, and destroy any OpenCV windows.
                             pygame.quit()
                             cv2.destroyAllWindows()
-                            return
 
+                            # Forcibly terminate the whole process. This isn't graceful, but ORB-SLAM can sometimes
+                            # take a long time to shut down, and it's dull to wait for it.
+                            # noinspection PyProtectedMember
+                            os._exit(0)
+
+                    # Get the current RGB-D image pair.
                     colour_image, depth_image = camera.get_images()
+
+                    # Use OpenPose to detect 2D skeletons in the colour image.
                     skeletons_2d, output_image = skeleton_detector.detect_skeletons_2d(colour_image)
 
+                    # Determine the detection pose to use. If tracking is in use and available, this will be
+                    # given by the tracker; if not, we simply use the identity matrix.
                     detection_pose: np.ndarray = np.eye(4)
                     if tracker is not None and tracker.is_ready():
                         tracker_c_t_w: Optional[np.ndarray] = tracker.estimate_pose(colour_image, depth_image)
                         if tracker_c_t_w is not None:
                             detection_pose = np.linalg.inv(tracker_c_t_w)
 
+                    # Compute the world-space points image and the mask indicating which pixels are valid.
                     ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
                         depth_image, detection_pose, camera.get_depth_intrinsics()
                     )
 
                     mask: np.ndarray = np.where(depth_image != 0, 255, 0).astype(np.uint8)
 
+                    # Lift the 2D skeletons into 3D.
                     skeletons_3d: List[SkeletonDetector.Skeleton] = skeleton_detector.lift_skeletons_to_3d(
                         skeletons_2d, ws_points, mask
                     )
 
+                    # If there's only one skeleton in the frame:
+                    # TODO: Once we can associate skeletons between frames, this limitation can be relaxed.
                     if len(skeletons_3d) == 1:
+                        # Use the skeleton to update the expected bone lengths.
                         bone_length_estimator.add_estimates(skeletons_3d[0])
 
+                        # Get the expected bone lengths, and print them out.
                         expected_bone_lengths: Dict[Tuple[str, str], float] = \
                             bone_length_estimator.get_expected_bone_lengths()
 
                         print(expected_bone_lengths)
 
-                        skeletons_3d = [
-                            SkeletonDetector.remove_bad_bones(skeleton, expected_bone_lengths) for skeleton in skeletons_3d
-                        ]
+                        # Remove any bones that have unexpected lengths from the skeleton.
+                        skeletons_3d[0] = SkeletonDetector.remove_bad_bones(skeletons_3d[0], expected_bone_lengths)
 
+                    # Blend the OpenPose output image with the depth image.
                     depth_image_uc: np.ndarray = np.clip(depth_image * 255 / 5, 0, 255).astype(np.uint8)
                     blended_image: np.ndarray = np.zeros(colour_image.shape, dtype=np.uint8)
                     for i in range(3):
                         blended_image[:, :, i] = (output_image[:, :, i] * 0.5 + depth_image_uc * 0.5).astype(np.uint8)
-                    cv2.imshow("2D OpenPose Result", blended_image)
 
-                    c: int = cv2.waitKey(1)
-                    if c == ord("q"):
-                        break
+                    # Show the result to aid debugging.
+                    cv2.imshow("2D OpenPose Result", blended_image)
+                    cv2.waitKey(1)
 
                     # Allow the user to control the camera.
                     camera_controller.update(pygame.key.get_pressed(), timer() * 1000)
@@ -133,11 +153,6 @@ def main() -> None:
 
                     # Swap the front and back buffers.
                     pygame.display.flip()
-
-                # Forcibly terminate the whole process. This isn't graceful, but ORB-SLAM can sometimes take a
-                # long time to shut down, and it's dull to wait for it.
-                # noinspection PyProtectedMember
-                os._exit(0)
     except RuntimeError as e:
         print(e)
 
