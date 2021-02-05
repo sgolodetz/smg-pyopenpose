deaa2ec86c0dc5dd1d89330c8797c6986d11a660
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-05 14:30:56 +0000

    Try using RGB-D camera tracking.

--- examples/mine/try_skeleton_detector.py
+++ examples/mine/try_skeleton_detector.py
@@ -7,11 +7,12 @@ import pygame
 
 from OpenGL.GL import *
 from timeit import default_timer as timer
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 from smg.opengl import OpenGLMatrixContext, OpenGLUtil
 from smg.openni import OpenNICamera
 from smg.pyopenpose import BoneLengthEstimator, SkeletonDetector, SkeletonRenderer
+from smg.pyorbslam2 import RGBDTracker
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.controllers import KeyboardCameraController
 from smg.rigging.helpers import CameraPoseConverter
@@ -39,76 +40,90 @@ def main() -> None:
     params: Dict[str, Any] = {"model_folder": "D:/openpose-1.6.0/models/"}
     with SkeletonDetector(params) as skeleton_detector:
         with OpenNICamera(mirror_images=True) as camera:
-            bone_length_estimator: BoneLengthEstimator = BoneLengthEstimator()
+            with RGBDTracker(
+                settings_file=f"settings-kinect.yaml", use_viewer=True,
+                voc_file="C:/orbslam2/Vocabulary/ORBvoc.txt", wait_till_ready=True
+            ) as tracker:
+                bone_length_estimator: BoneLengthEstimator = BoneLengthEstimator()
 
-            while True:
-                # Process any PyGame events.
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        cv2.destroyAllWindows()
-                        return
+                while True:
+                    # Process any PyGame events.
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            cv2.destroyAllWindows()
+                            return
 
-                colour_image, depth_image = camera.get_images()
-                skeletons_2d, output_image = skeleton_detector.detect_skeletons_2d(colour_image)
+                    colour_image, depth_image = camera.get_images()
+                    skeletons_2d, output_image = skeleton_detector.detect_skeletons_2d(colour_image)
 
-                ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
-                    depth_image, np.eye(4), camera.get_depth_intrinsics()
-                )
+                    # If the tracker's not yet ready, or the pose can't be estimated for this frame, continue.
+                    if not tracker.is_ready():
+                        continue
 
-                mask: np.ndarray = np.where(depth_image != 0, 255, 0).astype(np.uint8)
+                    tracker_c_t_w: Optional[np.ndarray] = tracker.estimate_pose(colour_image, depth_image)
+                    if tracker_c_t_w is None:
+                        continue
 
-                skeletons_3d: List[SkeletonDetector.Skeleton] = skeleton_detector.lift_skeletons_to_3d(
-                    skeletons_2d, ws_points, mask
-                )
+                    tracker_w_t_c: np.ndarray = np.linalg.inv(tracker_c_t_w)
 
-                if len(skeletons_3d) == 1:
-                    bone_length_estimator.add_estimates(skeletons_3d[0])
+                    ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
+                        depth_image, tracker_w_t_c, camera.get_depth_intrinsics()
+                    )
 
-                    expected_bone_lengths: Dict[Tuple[str, str], float] = \
-                        bone_length_estimator.get_expected_bone_lengths()
+                    mask: np.ndarray = np.where(depth_image != 0, 255, 0).astype(np.uint8)
 
-                    print(expected_bone_lengths)
+                    skeletons_3d: List[SkeletonDetector.Skeleton] = skeleton_detector.lift_skeletons_to_3d(
+                        skeletons_2d, ws_points, mask
+                    )
 
-                    skeletons_3d = [
-                        SkeletonDetector.remove_bad_bones(skeleton, expected_bone_lengths) for skeleton in skeletons_3d
-                    ]
+                    if len(skeletons_3d) == 1:
+                        bone_length_estimator.add_estimates(skeletons_3d[0])
 
-                depth_image_uc: np.ndarray = np.clip(depth_image * 255 / 5, 0, 255).astype(np.uint8)
-                blended_image: np.ndarray = np.zeros(colour_image.shape, dtype=np.uint8)
-                for i in range(3):
-                    blended_image[:, :, i] = (output_image[:, :, i] * 0.5 + depth_image_uc * 0.5).astype(np.uint8)
-                cv2.imshow("2D OpenPose Result", blended_image)
+                        expected_bone_lengths: Dict[Tuple[str, str], float] = \
+                            bone_length_estimator.get_expected_bone_lengths()
 
-                c: int = cv2.waitKey(1)
-                if c == ord("q"):
-                    break
+                        print(expected_bone_lengths)
 
-                # Allow the user to control the camera.
-                camera_controller.update(pygame.key.get_pressed(), timer() * 1000)
+                        skeletons_3d = [
+                            SkeletonDetector.remove_bad_bones(skeleton, expected_bone_lengths) for skeleton in skeletons_3d
+                        ]
 
-                # Clear the colour and depth buffers.
-                glClearColor(1.0, 1.0, 1.0, 1.0)
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+                    depth_image_uc: np.ndarray = np.clip(depth_image * 255 / 5, 0, 255).astype(np.uint8)
+                    blended_image: np.ndarray = np.zeros(colour_image.shape, dtype=np.uint8)
+                    for i in range(3):
+                        blended_image[:, :, i] = (output_image[:, :, i] * 0.5 + depth_image_uc * 0.5).astype(np.uint8)
+                    cv2.imshow("2D OpenPose Result", blended_image)
 
-                # Set the projection matrix.
-                with OpenGLMatrixContext(GL_PROJECTION, lambda: OpenGLUtil.set_projection_matrix(
-                    camera.get_colour_intrinsics(), *window_size
-                )):
-                    # Set the model-view matrix.
-                    with OpenGLMatrixContext(GL_MODELVIEW, lambda: OpenGLUtil.load_matrix(
-                        CameraPoseConverter.pose_to_modelview(camera_controller.get_pose())
+                    c: int = cv2.waitKey(1)
+                    if c == ord("q"):
+                        break
+
+                    # Allow the user to control the camera.
+                    camera_controller.update(pygame.key.get_pressed(), timer() * 1000)
+
+                    # Clear the colour and depth buffers.
+                    glClearColor(1.0, 1.0, 1.0, 1.0)
+                    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+                    # Set the projection matrix.
+                    with OpenGLMatrixContext(GL_PROJECTION, lambda: OpenGLUtil.set_projection_matrix(
+                        camera.get_colour_intrinsics(), *window_size
                     )):
-                        # Render a voxel grid.
-                        glColor3f(0.0, 0.0, 0.0)
-                        OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
+                        # Set the model-view matrix.
+                        with OpenGLMatrixContext(GL_MODELVIEW, lambda: OpenGLUtil.load_matrix(
+                            CameraPoseConverter.pose_to_modelview(camera_controller.get_pose())
+                        )):
+                            # Render a voxel grid.
+                            glColor3f(0.0, 0.0, 0.0)
+                            OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
 
-                        # Render the 3D skeletons.
-                        for skeleton_3d in skeletons_3d:
-                            SkeletonRenderer.render_skeleton(skeleton_3d)
+                            # Render the 3D skeletons.
+                            for skeleton_3d in skeletons_3d:
+                                SkeletonRenderer.render_skeleton(skeleton_3d)
 
-                # Swap the front and back buffers.
-                pygame.display.flip()
+                    # Swap the front and back buffers.
+                    pygame.display.flip()
 
 
 if __name__ == "__main__":
